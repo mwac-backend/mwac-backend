70cfa07626906f8b28faca86f366c2b6c7d91e09
Supaat Wisetsud <600112416008@bru.ac.th>
2021-08-23 20:35:39 +0700

    socket message chat v1

---
+++ controllerV1/socket/room.socket.controller.js
@@ -0,0 +1,34 @@
+const { ScoketData } = require('./sockData.repository');
+const { authValidation } = require('../../middleware/auth.validation.middleware');
+
+module.exports.roomSocket = {
+    roomSocketController,
+}
+
+function roomSocketController(io) {
+    io.use(authValidation.validJWTSocketNeeded);
+    io.on('connection', (socket) => {
+
+        const user = socket.user;
+        socket.emit('connect-info', socket.user);
+
+        socket.emit('get-rooms', async (data) => {
+            const rooms = await ScoketData.getRoom({user});
+            socket.emit('on-get-rooms', rooms);
+        })
+
+        socket.emit('create-room', async (data) => {
+            let { agencyID, submissionControlID, remark } = data || {};
+
+            const result = await ScoketData.updateRoom({
+                id: null,
+                agencyID, 
+                submissionControlID, 
+                remark, 
+                user
+            });
+            socket.emit('on-create-room', result);
+        });
+
+    });
+}
